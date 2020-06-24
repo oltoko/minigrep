319151705e753088e4d37c8a9f25fc0d5a72f3e8
Oliver Koch <github@oltoko.de>
2020-06-24 22:35:02 +0200

    using iterators and closures

--- src/lib.rs
+++ src/lib.rs
@@ -22,30 +22,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     let query = &query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query))
+        .collect()
 }
 
 pub struct Config {
@@ -55,14 +45,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
 
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name")
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config { query, filename, case_sensitive })
@@ -73,40 +67,6 @@ impl Config {
 mod tests {
 
     use super::*;
-
-    #[test]
-    fn new_config() {
-        let args = &["minigrep".to_string(), "query".to_string(), "file".to_string()];
-        let config = Config::new(args);
-
-        assert!(config.is_ok());
-        let config = config.unwrap();
-
-        assert_eq!(config.query, "query");
-        assert_eq!(config.filename, "file");
-        assert_eq!(config.case_sensitive, true);
-    }
-
-    #[test]
-    fn new_config_4_args() {
-        let args = &["minigrep".to_string(), "query".to_string(), "file".to_string(), "to-much".to_string()];
-        let config = Config::new(args);
-
-        assert!(config.is_ok());
-        let config = config.unwrap();
-
-        assert_eq!(config.query, "query");
-        assert_eq!(config.filename, "file");
-        assert_eq!(config.case_sensitive, true);
-    }
-
-    #[test]
-    fn new_config_2_args() {
-        let args = &["minigrep".to_string(), "query".to_string()];
-        let config = Config::new(args);
-
-        assert!(config.is_err());
-    }
 
     #[test]
     fn case_sensitive() {
